d89044512a61abfb9720102dd38c23f556f3c43a
Aline Sotte <alinedpaso@gmail.com>
2024-11-17 14:55:52 -0300

    Questão 1

---
+++ Questao_1/uf.sql
@@ -0,0 +1,22 @@
+
+use eleicao 
+
+GO
+
+CREATE TABLE uf (
+    SG_UE_CH INT IDENTITY(1,1) PRIMARY KEY,
+    SG_UE VARCHAR(2),
+    NM_UE NVARCHAR(255),
+    SG_UF CHAR(2)
+);
+
+WAITFOR DELAY '00:00:05';
+
+INSERT INTO uf (SG_UE, NM_UE,  SG_UF )
+SELECT DISTINCT
+    SG_UE,
+    NM_UE,
+    SG_UF
+FROM dbo.consulta_cand_2024_BRASIL 
+
+GO
